6c4c43490ba8bfbc98907caeacc89a7f6b878f1f
Beatriz <btrcardos@gmail.com>
2022-10-08 15:59:59 -0300

    DB Connect and Includes

--- routes/users.js
+++ routes/users.js
@@ -4,16 +4,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
   
-  conn.query("SELECT * FROM tb_users ORDER BY name", (err,results) => {
+  conn.query(" SELECT * FROM tb_users ORDER BY name", (err,results) => {
     if (err){
       res.send(err);
     } else {
       res.send(results);
     }
   });
-
+  
 });
+
+
 
 module.exports = router;
