19b6a5ee1d1046c854ee33d8abce06441e531324
Beatriz <btrcardos@gmail.com>
2022-10-21 22:57:44 -0300

    Admin create reservation

--- inc/reservations.js
+++ inc/reservations.js
@@ -19,20 +19,47 @@ module.exports = {
 
         return new Promise((resolve, reject)=>{
 
-            let date = fields.date.split('/');
-            date = `${date[2]}-${date[1]}-${date[0]}`;
+            let date = fields.date;
 
-            // conn.query(sql string, vector data, callback)
-            conn.query(`
-                INSERT INTO tb_reservations (name, email, people, date, time)
-                VALUES ( ?, ?, ?, ?, ?)
-            `, [
+            if(fields.date.indexOf('/') > -1){
+                date = fields.date.split('/');
+                date = `${date[2]}-${date[1]}-${date[0]}`;
+            }
+
+            let query, params = [
                 fields.name, 
                 fields.email, 
                 fields.people, 
                 date, 
                 fields.time
-            ], (err, results) => {
+            ];
+
+            if(parseInt(fields.id) > 0){
+
+                query = `
+                    UPDATE tb_reservations
+                    SET 
+                        name = ?,
+                        email = ?,
+                        people = ?,
+                        date = ?, 
+                        time = ?
+                    WHERE id = ?
+                `;
+
+                params.push(fields.id);
+
+            } else {
+
+                query = `
+                    INSERT INTO tb_reservations (name, email, people, date, time)
+                    VALUES ( ?, ?, ?, ?, ?)
+                `;
+
+            }
+
+            // conn.query(sql string, vector data, callback)
+            conn.query(query, params, (err, results) => {
                 
                 if(err){
                     reject(err);
