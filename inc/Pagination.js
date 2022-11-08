6c6cefef6ffa494a9b03e9c3ce2e2d8cf4d7c53e
Beatriz <btrcardos@gmail.com>
2022-11-07 23:41:29 -0300

    Pagination

--- inc/Pagination.js
+++ inc/Pagination.js
@@ -81,6 +81,13 @@ class Pagination {
             nrend = this.getCurrentPage() + parseInt(limitPagesNav/2);
         }
 
+        if(this.getCurrentPage() > 1){
+            links.push({
+                text: '<',
+                href: '?' + this.getQueryString( Object.assign({}, params, {page: this.getCurrentPage() - 1}) )
+            });
+        }
+
         for (let x = nrstart; x<=nrend; x++){
 
             links.push({
@@ -89,6 +96,13 @@ class Pagination {
                 active: (x === this.getCurrentPage())
             })
 
+        }
+
+        if(this.getCurrentPage() < this.getTotalPages()){
+            links.push({
+                text: '>',
+                href: '?' + this.getQueryString( Object.assign({}, params, {page: this.getCurrentPage() + 1}) )
+            });
         }
 
         return links;
